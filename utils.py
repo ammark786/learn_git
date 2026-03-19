2939189bba440f96812867c6873dd86c0c9b3a2e
ammar khan <ammar.hhh22@gmail.com>
2026-03-19 19:45:56 +0400

    My change: greet message variant A

--- utils.py
+++ utils.py
@@ -3,8 +3,7 @@
 
 def greet(name: str) -> str:
     """Return a greeting string."""
-    return f"Hey, {name}! Great to see you."
-
+    return f"Hi, {name}! Great to see you."
 
 def format_number(n: float) -> str:
     """Format a number with 2 decimal places."""
